6d901f2fd5d5349faf9f2fbd1d4290487ccaa581
jonndoe <qb89@yandex.ru>
2020-04-30 22:23:24 +0300

    comments styling added, works

--- bakerydemo/settings/base.py
+++ bakerydemo/settings/base.py
@@ -184,12 +184,8 @@ WAGTAIL_SITE_NAME = "bakerydemo"
 
 
 # django-comments-xtd
-SITE_ID = 1
 COMMENTS_APP = 'django_comments_xtd'
-COMMENTS_XTD_MAX_THREAD_LEVEL = 2
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
+SITE_ID = 1
 #  To help obfuscating comments before they are sent for confirmation.
 COMMENTS_XTD_SALT = (b"Timendi causa est nescire. "
                      b"Aequam memento rebus in arduis servare mentem.")
@@ -199,3 +195,15 @@ COMMENTS_XTD_FROM_EMAIL = "noreply@example.com"
 
 # Contact mail address to show in messages.
 COMMENTS_XTD_CONTACT_EMAIL = "helpdesk@example.com"
+
+COMMENTS_XTD_MAX_THREAD_LEVEL = 1  # default is 0
+COMMENTS_XTD_LIST_ORDER = ('-thread_id', 'order')  # default is ('thread_id', 'order')
+
+
+COMMENTS_XTD_APP_MODEL_OPTIONS = {
+    'blog.blogpage': {
+        'allow_flagging': True,
+        'allow_feedback': True,
+        'show_feedback': True,
+    }
+}
